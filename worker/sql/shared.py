706f55a50393f9ada3e986594755290a181d1499
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2021-02-12 21:48:27 +0000

    add some shared util.

---
+++ worker/sql/shared.py
@@ -0,0 +1,26 @@
+from environs import Env
+
+env = Env()
+env.read_env()
+
+from mysql.connector import connect
+from path import join, dirname, realpath
+
+def get_connection():
+	return mysql.connector.connect(
+		user=env.str("MYSQL_USER", "root"),
+		password=env.str("MYSQL_PASSWORD", ""),
+		host=env.str("MYSQL_HOST", "db"),
+		database=env.str("MYSQL_DATABASE", "photon")
+	)
+
+def load_file(id, path="migrations")
+	path = join(dirname(realpath(__file__)), path, id + ".sql")
+	with file as open(path, "r"):
+		return file.read()
+
+def load_migration(id):
+	return load_file(id)
+
+def load_statement(id):
+	return load_file(id, "statements")
