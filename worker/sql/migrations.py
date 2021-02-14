9fc875990619c4f7143c21fbbed55d880865e6c1
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2021-02-14 14:13:56 +0000

    reformat & fixes.

--- worker/sql/migrations.py
+++ worker/sql/migrations.py
@@ -1,45 +1,49 @@
 from os import listdir
-from os.path import isfile, join, splitext
-from .shared import load_file
+from os.path import isfile, join, splitext, dirname, realpath
+from .shared import load_file, get_connection
+
 
 def load_migration(id):
-	return load_file(id)
+    return load_file(id)
+
 
 def migration_ran(id):
-	con = get_connection()
-	curs = con.cursor()
-	try:
-		data = curs.execute("SELECT * FROM migrations WHERE mid = %s", (id,))
-		return id == "init" and True or (len(data) > 0)
-	except:
-		return False
+    con = get_connection()
+    curs = con.cursor()
+    try:
+        data = curs.execute("SELECT * FROM migrations WHERE mid = %s", (id,))
+        return id == "init" and True or (len(data) > 0)
+    except:
+        return False
+
 
 def migration(id, curs):
-	curs.execute(load_migration(id))
+    curs.execute(load_migration(id))
+
 
 def migrate():
-	con = get_connection()
-	curs = con.cursor()
+    con = get_connection()
+    curs = con.cursor()
 
-	curs.execute("BEGIN TRANSACTION;")
+    curs.execute("BEGIN TRANSACTION;")
 
-	base_path = join(dirname(realpath(__file__)), "migrations")
-	migrations = [file for file in listdir(base_path) if isfile(join(base_path, file))]
-	migrations.sort()
+    base_path = join(dirname(realpath(__file__)), "migrations")
+    migrations = [file for file in listdir(base_path) if isfile(join(base_path, file))]
+    migrations.sort()
 
-	try:
-		if not migration_ran("init"):
-			migration("init", curs)
+    try:
+        if not migration_ran("init"):
+            migration("init", curs)
 
-		for migration in migrations.values():
-			pair = splitext(migration)
-			id = pair[0]
-			if not migration_ran(id):
-				migration(id, curs)
-	except:
-		curs.execute("ROLLBACK;")
-		raise
-	else:
-		curs.execute("COMMIT;")
-	finally:
-		curs.close()
+        for mig in migrations.values():
+            pair = splitext(mig)
+            id = pair[0]
+            if not migration_ran(id):
+                migration(id, curs)
+    except:
+        curs.execute("ROLLBACK;")
+        raise
+    else:
+        curs.execute("COMMIT;")
+    finally:
+        curs.close()
