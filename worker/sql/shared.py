b5b794b1c71f2391dc7c5c081f9f55f9a23d8d7e
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2021-02-14 14:12:52 +0000

    Reformat and fix shared.

--- worker/sql/shared.py
+++ worker/sql/shared.py
@@ -4,25 +4,31 @@ env = Env()
 env.read_env()
 
 from mysql.connector import connect
-from path import join, dirname, realpath
+from os.path import join, dirname, realpath
+
 
 def get_connection():
-	return mysql.connector.connect(
-		user=env.str("MYSQL_USER", "root"),
-		password=env.str("MYSQL_PASSWORD", ""),
-		host=env.str("MYSQL_HOST", "db"),
-		database=env.str("MYSQL_DATABASE", "photon")
-	)
+    return connect(
+        user=env.str("MYSQL_USER", "root"),
+        password=env.str("MYSQL_PASSWORD", ""),
+        host=env.str("MYSQL_HOST", "db"),
+        database=env.str("MYSQL_DATABASE", "photon")
+    )
 
-def load_file(id, path="migrations")
-	path = join(dirname(realpath(__file__)), path, id + ".sql")
-	with file as open(path, "r"):
-		return file.read()
 
-def load_statement(id, data = {}, path="migrations"):
-	content = load_file(id, path)
-	for key in data.keys():
-		k = "{{" + key + "}}"
-		v = data[key]
-		content = content.replace(k, v)
-	return content
+def load_file(id, path="migrations"):
+    path = join(dirname(realpath(__file__)), path, id + ".sql")
+    with open(path, "r") as file:
+        return file.read()
+
+
+def load_statement(id, data=None, path="migrations"):
+    if data is None:
+        data = {}
+
+    content = load_file(id, path)
+    for key in data.keys():
+        k = "{{" + key + "}}"
+        v = data[key]
+        content = content.replace(k, v)
+    return content
